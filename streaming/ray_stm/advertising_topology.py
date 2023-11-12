72411446a3c6a6a59a1c583ae54e3a804de8a8de
Yuhang Zhang <repeater9@163.com>
2023-11-12 21:42:35 +0800

    implement operators

--- streaming/ray_stm/advertising_topology.py
+++ streaming/ray_stm/advertising_topology.py
@@ -1,5 +1,7 @@
 # Standard library imports
-
+from typing import Tuple
+import json
+import logging
 
 # Third party imports
 import ray
@@ -10,30 +12,52 @@ from ray_stm.advertising import CampaignProcessorCommon, RedisAdCampaignCache
 
 
 @ray.remote
-def DeserializeBolt(input):
-    return tuple()
+def DeserializeBolt(input: str):
+    obj = json.loads(input)
+    user_id = obj.get("user_id")
+    page_id = obj.get("page_id")
+    ad_id = obj.get("ad_id")
+    ad_type = obj.get("ad_type")
+    event_type = obj.get("event_type")
+    event_time = obj.get("event_time")
+    ip_address = obj.get("ip_address")
+    return user_id, page_id, ad_id, ad_type, event_type, event_time, ip_address
 
 
 @ray.remote
-def EventFilterBolt(input):
-    return tuple()
+def EventFilterBolt(input: Tuple[str]):
+    if input[4] == "view":
+        return input
+    return None
 
 
 @serve.deployment
 class RedisJoinBolt(object):
-    def __init__(self, redis_host, redis_port):
-        self.redis_ad_campaign_cache = RedisAdCampaignCache(redis_host, redis_port)
-        pass
+    logger = logging.getLogger("RedisJoinBolt")
 
-    def flat_map(tuple):
-        pass
+    def __init__(self, redis_host, redis_port):
+        self.logger.info(f"Opening redis connection to {redis_host}:{redis_port}")
+        self.redis_ad_campaign_cache = RedisAdCampaignCache(redis_host, redis_port)
+
+    def flat_map(self, input: Tuple[str]):
+        ad_id = input[0]
+        campaign_id = self.redis_ad_campaign_cache.execute(ad_id)
+        if campaign_id == None:
+            return None
+
+        return campaign_id, input[0], input[1]
 
 
 @serve.deployment
 class CampaignProcessor(object):
+    logger = logging.getLogger("CampaignProceesor")
+
     def __init__(self, redis_host, redis_port):
+        self.logger.info(f"Opening redis connection to {redis_host}:{redis_port}")
         self.processor = CampaignProcessorCommon(redis_host, redis_port)
         self.processor.prepare()
 
-    def flat_map(tuple):
-        pass
+    def flat_map(self, input: Tuple[str]):
+        campaign_id = input[0]
+        event_time = input[2]
+        self.processor.execute(campaign_id, event_time)
--- streaming/ray_stm/main.py
+++ streaming/ray_stm/main.py
@@ -1,5 +1,5 @@
 # Standard library imports
-
+import json
 
 # Third party imports
 import ray
@@ -18,5 +18,18 @@ if __name__ == "__main__":
     config = load_yaml_config("config.yaml")
     redis_host = config["redis.host"]
     redis_port = config["redis.port"]
-    print(redis_host, redis_port)
+    input = json.dumps(
+        {
+            "user_id": "1",
+            "page_id": "2",
+            "ad_id": "3",
+            "ad_type": "view",
+            "event_type": "view",
+            "event_time": "12313123",
+            "ip_address": "192.168.10.10",
+        }
+    )
+
+    ref = DeserializeBolt.remote(input)
+    print(ray.get(ref))
     # processor = CampaignProcessor.bind(kafka_host, kafka_port)
